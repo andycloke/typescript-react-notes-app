b794127c2864b61070e3fca1a1a3e8c9c422b1a1
andycloke <acploke@gmail.com>
2019-09-25 09:52:06 +0100

    Integrate agasint actual service

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,61 +1,26 @@
+import axios from 'axios';
 import { NotesResponse, NoteResponse, NotePostRequest, NotePatchRequest } from '../types';
-
-const timeout = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-const timeout1s = () => timeout(500);
-
 class NoteService {
-  notes: NotesResponse = [];
+  service = axios.create({ baseURL: `${process.env.REACT_APP_BASE_URL}/notes` });
 
   getNotes = async (): Promise<NotesResponse> => {
-    await timeout1s();
-    return this.notes;
+    return (await this.service.get<NotesResponse>('')).data;
   };
 
   getNote = async (id: string): Promise<NoteResponse> => {
-    await timeout1s();
-    const note = this.notes.find(note => note.id === id);
-    if (note) {
-      return note;
-    } else {
-      throw new Error(`Note with id: ${id} 'does not exist`);
-    }
+    return (await this.service.get<NoteResponse>(`/${id}`)).data;
   };
 
   postNote = async (body: NotePostRequest) => {
-    await timeout1s();
-    const newNote: NoteResponse = {
-      id: Math.round(Math.random() * 100000).toString(),
-      createdAt: new Date().toString(),
-      updatedAt: new Date().toString(),
-      ...body
-    };
-    this.notes.push(newNote);
-    return newNote;
+    return (await this.service.post<NoteResponse>('', body)).data;
   };
 
   patchNote = async (id: string, body: NotePatchRequest) => {
-    await timeout1s();
-    const existingNote = this.notes.find(note => note.id === id);
-    if (existingNote) {
-      const updatedNote: NoteResponse = {
-        ...existingNote,
-        ...body
-      };
-      this.notes = this.notes.map(note => (note.id === id ? updatedNote : note));
-      return updatedNote;
-    } else {
-      throw new Error(`Note with id: ${id} 'does not exist`);
-    }
+    return (await this.service.patch<NoteResponse>(`/${id}`, body)).data;
   };
 
   deleteNote = async (id: string) => {
-    await timeout1s();
-    const existingNote = this.notes.find(note => note.id === id);
-    if (existingNote) {
-      this.notes = this.notes.filter(note => note.id !== id);
-    } else {
-      throw new Error(`Note with id: ${id} 'does not exist`);
-    }
+    return this.service.delete(`/${id}`);
   };
 }
 
